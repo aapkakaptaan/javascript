7d2c0a3a6f29815bc48cb6447c8eedb49481cec4
aapkakaptaan <149105860+aapkakaptaan@users.noreply.github.com>
2024-06-30 07:58:54 +0000

    datatypes

---
+++ 01_baics/02_datatpes.js
@@ -0,0 +1,22 @@
+
+"use strict";// treat all JS code as newer version
+
+//alert(3+3)  ,we are using nodejs,not browser
+
+console.log(3+3);
+console.log("Siddhant")
+
+let name="hitesh"
+let age=18
+let isLoggedIn=true
+let state=null;
+// number=>2 to power 53
+// bigint
+//boolean=> true/false
+//string=>""
+//null=? stand alone value
+//undefined=> 
+//symbol => unique
+//object
+
+console.log(typeof null)// object
--- 01_baics/test.js
+++ 01_baics/test.js
@@ -1,1 +1,18 @@
-console.log("Siddhant")
+const accountID=14453
+let email="sid@create"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+//accountID=2
+email="sid@create.com"
+console.log(accountID);
+
+/*
+prefer not to use vae
+because of issue in block scope and functional scope
+
+*/
+
+
+console.table([accountID,email,accountCity,accountState]) 
