45ef602536098bab3d320c23243d9572698f745e
aapkakaptaan <149105860+aapkakaptaan@users.noreply.github.com>
2024-06-30 10:20:00 +0000

    strings

--- 01_baics/strings.js
+++ 01_baics/strings.js
@@ -1,1 +1,31 @@
 const name="sid"
+const repoCount=50
+
+// console.log(name+ repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repo is ${repoCount}`);
+
+const gameName=new String("kaptaan")
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('a'));
+
+const newString=gameName.substring(0,3)
+console.log(newString);
+
+const newS="       SIDDHANT"
+console.log(newS)
+console.log(newS.trim());
+
+const url="https://siddhant.com/siddhant%20Rai"
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('siddhant'))
+
+console.log(url.split('-'))
