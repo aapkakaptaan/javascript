8959e401a5b0ea24850984e9874ca47b5295ec61
aapkakaptaan <149105860+aapkakaptaan@users.noreply.github.com>
2024-06-30 08:10:49 +0000

    conversion of datatype

---
+++ 01_baics/03_conversion.js
@@ -0,0 +1,18 @@
+let score=null
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber=Number(score)
+
+console.log(valueInNumber)
+
+//"33"=>33
+//"33a"=> NaN
+// true=1
+// null=0
+//undefined=undefined
+
+let SignedIn=1
+let BooleanIsLoggedIn=Boolean(SignedIn)
+console.log(BooleanIsLoggedIn)
