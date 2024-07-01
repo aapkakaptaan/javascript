8f9738a66ea4f55ccc14c71dd36105b7b527ea94
aapkakaptaan <149105860+aapkakaptaan@users.noreply.github.com>
2024-07-01 20:57:50 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,39 @@
+const myArray=[0,1,3,4,5,"hitesh"]
+const heores=["shaktimaan","naagraaj"]
+
+const myArray2=new Array(1,2,3,4,5)
+// console.log(myArray[1]);
+
+//arary methods
+
+// myArray.push(6)
+
+// // myArray.push(7)
+
+// myArray.pop()
+
+
+// myArray.unshift("siddhant")
+
+// myArray.shift()
+
+// myArray.shift()
+
+// const newArr=myArray.join()
+
+// console.log(myArray);
+// console.log(typeof newArr);
+
+
+//slice,splice
+
+console.log("A",myArray2)
+
+const myn1=myArray2.slice(1,3)
+console.log(myn1);
+console.log("b",myArray2);
+
+
+const myn2=myArray2.splice(1,3)
+console.log(myn2);
+console.log("b",myArray2);
